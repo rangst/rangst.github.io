92a21debbcd38a03f3d2c9557a913802c9957748
Roland Angst <angst.roland@gmail.com>
2015-05-11 18:28:21 +0200

    Initial commit of first version.

---
+++ media/2008-PG/PG08swf.php
@@ -0,0 +1,20 @@
+<?php include ("../../MasterTemplate.php"); ?>
+
+
+<?php BeginContentArea(); ?>
+
+<h1> Pacific Graphics 2008 Presentation </h1>
+
+<!-- Use the following <object> tag to embed the Flash presentation into HTML document -->
+<object id="presentation" width="600" height="400" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" align="middle">
+<param name="allowScriptAccess" value="sameDomain" />
+<param name="movie" value="Session4.swf" />
+
+<param name="quality" value="high" />
+<param name="bgcolor" value="#ffffff" />
+<param name="allowFullScreen" value="true" />
+<embed src="./PG08.swf" quality="high" bgcolor="#ffffff" width="600" height="400" name="presentation" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" allowFullScreen="true" />
+</object>
+
+
+<?php EndContentArea(); ?>
